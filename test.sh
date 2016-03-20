52557f9e0326235b1f82baf2dacfc0a39f87e04e
mpeterv <mpeterval@gmail.com>
2016-03-20 22:44:47 +0300

    Attempt 3 to test on travis OS X

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@ set -ev
 export PATH="$PWD/test/here/bin:$PATH"
 HEREROCKS="python hererocks.py test/here --downloads=test/cache --no-git-cache"
 
-rm test/here -rf
+rm -rf test/here
 $HEREROCKS -l^ -r^
 lua -v
 luarocks --version
@@ -11,7 +11,7 @@ luarocks make
 hererocks-test | grep "5\.3"
 $HEREROCKS -l^ -r^ | grep "already installed"
 
-rm test/here -rf
+rm -rf test/here
 $HEREROCKS -j @v2.1 -r^ | grep "Fetching" | grep "cached"
 lua -v
 lua -e "require 'jit.bcsave'"
@@ -19,15 +19,15 @@ luarocks --version
 luarocks make
 hererocks-test | grep "2\.1"
 
-rm test/here -rf
+rm -rf test/here
 $HEREROCKS -l 5.1 --compat=none --no-readline
 lua -e "assert(not pcall(string.gfind, '', '.'))"
 lua -e "(function(...) assert(arg == nil) end)()"
 lua -e "assert(math.mod == nil)"
 
-rm test/here -rf
+rm -rf test/here
 $HEREROCKS -l 5.3 --compat=none --builds=test/builds
 lua -e "assert(module == nil)"
 
-rm test/here -rf
+rm -rf test/here
 $HEREROCKS -l 5.3 --compat=none --builds=test/builds | grep "Building" | grep "cached"
