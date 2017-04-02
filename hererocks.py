15b8240a65f18c12d92770e9bf77599565696c8f
Peter Melnichenko <mpeterval@gmail.com>
2017-04-02 20:53:12 +0300

    0.15.0 release

--- hererocks.py
+++ hererocks.py
@@ -32,7 +32,7 @@ if os.name == "nt":
     except ImportError:
         import winreg
 
-hererocks_version = "Hererocks 0.14.0"
+hererocks_version = "Hererocks 0.15.0"
 __all__ = ["main"]
 
 opts = None
